c491f5a1347802def8fb5e1c389924524c8e7ea2
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-08-04 10:24:50 +0200

    Added favicon

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+
 from django.conf.urls import url
+from django.views.static import serve
 
 from .views import ShowTemplate
 
 
+IMG_DIR = os.path.join(os.path.dirname(__file__), os.pardir, 'static', 'img')
+
+
 urlpatterns = [
-	url('^$', ShowTemplate.as_view(), kwargs={'template': 'index.html'}),
-	url('^(?P<template>.*)$', ShowTemplate.as_view()),
+	url(r'^$', ShowTemplate.as_view(), kwargs={'template': 'index.html'}),
+	url(r'^(?P<path>favicon\.ico)$', serve, {'document_root': IMG_DIR}),
+	url(r'^(?P<template>.*)$', ShowTemplate.as_view()),
 ]
