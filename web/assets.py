a45b8bcea5e37fbe34ab130f1ad8222e0e1a7552
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-11-15 16:56:26 +0100

    Generating sprites from svg

--- web/assets.py
+++ web/assets.py
@@ -11,15 +11,13 @@ SPRITES = (
 		'name': 'sprites',
 		'output': 'img/sprites_generated.png',
 		'scss_output': 'css/_sprites_generated.scss',
-		#'extra_sizes': ((2, '@2x'),),
-		'extra_sizes': (),
+		'extra_sizes': ((2, '@2x'),),
 		'width': 256,
 		'height': 512,
 		'images': (
-			# large first
-			{ 'name': 'cpu', 'src': 'img/sprites/cpu.png' },
-			{ 'name': 'hdd', 'src': 'img/sprites/hdd.png' },
-			{ 'name': 'vol', 'src': 'img/sprites/vol.png' },
+			# place larger first
+			{ 'name': 'check', 'src': 'img/sprites/check.png' },
+			{ 'name': 'radio', 'src': 'img/sprites/radio.png' },
 		),
 	},
 )
