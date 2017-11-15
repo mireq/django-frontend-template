80c8f00061db001b52b9906ef2f19571b45f417b
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-11-15 16:30:31 +0100

    Fixed compiler error without psotcss

--- web/compress.py
+++ web/compress.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
+from compressor.exceptions import FilterError
 from compressor.filters import CompilerFilter
 from django.conf import settings
+
+
+logger = logging.getLogger('django.request')
 
 
 class PostcssFilter(CompilerFilter):
 	command = "{binary} {args}"
 	options = (
-		("binary", settings.COMPRESS_POSTCSS_BINARY),
-		("args", settings.COMPRESS_POSTCSS_ARGS),
+		("binary", getattr(settings, 'COMPRESS_POSTCSS_BINARY', 'postcss')),
+		("args", getattr(settings, 'COMPRESS_POSTCSS_ARGS', '--use autoprefixer')),
 	)
+
+	def input(self, **kwargs):
+		try:
+			return super(PostcssFilter, self).input(**kwargs)
+		except FilterError as e:
+			logger.exception("Compiler error")
+			return self.content
