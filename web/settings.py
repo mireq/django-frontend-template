7a1418dee7583571895927a7b59fba7e9048c247
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-11-16 18:15:50 +0100

    MIDDLEWARE_CLASSES are deprecated

--- web/settings.py
+++ web/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
 	'django_assets_manager',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
