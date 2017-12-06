ce1ac3f500e678640c73541f763b061cc19d7929
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-12-06 14:37:04 +0100

    Changed path to postcss binary

--- web/settings_sample.py
+++ web/settings_sample.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from .settings import *
 
 
-COMPRESS_POSTCSS_BINARY = '/usr/bin/postcss'
+COMPRESS_POSTCSS_BINARY = 'postcss' # path to postcss binary
 
 ALLOWED_HOSTS = ['*']
